9503ccea8aae51ab300b9e9b57c6d7e19108e4d1
Beo274 <romka24.rk@gmail.com>
2022-10-15 12:35:30 +0300

    Add file decryption

--- decipher/views.py
+++ decipher/views.py
@@ -1,6 +1,178 @@
+import hashlib
+import mimetypes
+import os, shutil, sys
+from wsgiref.util import FileWrapper
+from django.http import StreamingHttpResponse
 from django.shortcuts import render
+from Crypto.Cipher import AES
+from django.core.files.storage import FileSystemStorage
+from pathlib import Path
+from Crypto.Util.Padding import unpad
+from base64 import b64decode
 
-# Create your views here.
+
+def download_file(file_url):
+    """
+    Осуществялет скачивание файла с сервера на устройство пользователя
+    @param file_url: Путь к файлу на сервере
+    @return: Данные, необходимые для скачивания файла
+    """
+    file = file_url
+    file_name = os.path.basename(file)
+    chunk_size = 8192
+    response = StreamingHttpResponse(FileWrapper(open(file, 'rb'), chunk_size),
+                                     content_type=mimetypes.guess_type(file)[0])
+    response['Content-Length'] = os.path.getsize(file)
+    response['Content-Disposition'] = "attachment; filename=%s" % file_name
+    return response
+
+
+def file_save(decipher_data: bytes, file_url):
+    """
+    Осуществляет скачивание файла на сервер
+    @param decipher_data: Расшифрованные данные
+    @param file_url: Путь к файлу на сервере
+    @return: Данные, необходимые для скачивания файла
+    """
+    file_url = file_url[0:-10]
+    with open(file_url, 'wb') as newFile:
+        newFile.write(decipher_data)
+    return download_file(file_url)
+
+
+def xor_file_decryption(cipher_data: bytes, password, file_url):
+    """
+    Осуществляет расшифровку файла с помощью алгоритма XOR
+    @param cipher_data: Зашифрованные данные
+    @param password: Пароль, вводимый пользователем
+    @param file_url: Путь к файлу на сервере
+    @return: Данные, необходимые для скачивания файла
+    """
+    decrypted_data = []
+    for i in range(len(cipher_data)):
+        decrypted_data.append(cipher_data[i] ^ ord(password[i % len(password)]))
+    decipher_data = bytes(decrypted_data)
+    return file_save(decipher_data, file_url)
+
+
+def aes_file_decryption(cipher_data: bytes, password: str, file_url: str):
+    """
+    Осущетсвляет шифрование файла с помощью алгоритма AES
+    @param cipher_data: Зашифрованные данные
+    @param password: Пароль, вводимый пользователем
+    @param file_url: Путь к файлу на сервере
+    @return: Данные, необходимые для скачивания файла
+    """
+    try:
+        key = hashlib.sha256(password.encode('utf-8')).digest()
+        iv = cipher_data[:16]
+        encrypted_data = cipher_data[16:]
+        decipher = AES.new(key, AES.MODE_CBC, iv)
+        decipher_data = unpad(decipher.decrypt(encrypted_data), AES.block_size)
+        return file_save(decipher_data, file_url)
+    except (ValueError, KeyError) as e:
+        print(f"Incorrect decryption. Error: {e}")
+
+
+def file_processing(request, button_value: str):
+    """
+    Осуществляет получение данных из файла
+    @param request: Запрос
+    @param button_value: Значение нажатой кнопки
+    @return: Данные, необходимые для скачивания файла
+    """
+    upload = request.FILES.get('upload', False)
+    fss = FileSystemStorage()
+    file = fss.save(upload.name, upload)
+    file_url = fss.url(file)[1:]
+    with open(file_url, 'rb') as file:
+        data = file.read()
+    password = request.POST.get('filePassword')
+    if button_value == 'AESdcrptFile':
+        return aes_file_decryption(data, password, file_url)
+    elif button_value == 'XORdcrptFile':
+        return xor_file_decryption(data, password, file_url)
+
+
+def xor_text_decryption(encrypted_text: str, password: str):
+    """
+    Осуществляет рассшифровку текстовых данных с помощью алгоритма XOR
+    :param encrypted_text: зашифрованный текст
+    :param password: пароль для рассшифровки
+    :return: расшифрованный текст
+    """
+    try:
+        decrypted_data = []
+        for i in range(len(encrypted_text)):
+            decrypted_data.append(ord(encrypted_text[i]) ^ ord(password[i % len(password)]))
+        decrypted_text = ''
+        for i in range(len(decrypted_data)):
+            decrypted_text = decrypted_text + chr(decrypted_data[i])
+        return decrypted_text
+    except ZeroDivisionError:
+        return 'Not enough data to encrypt your text :(\nEnter some password'
+
+
+def aes_text_decryption(cipher_text: bytes, password: str):
+    """
+    Осуществляет расшифровку текста с помощью алгоритма AES
+    @param cipher_text: Зашифрованные текст
+    @param password: Пароль, вводимый пользователем
+    @return: Расшифрованный текст
+    """
+    try:
+        key = hashlib.sha256(password.encode('utf-8')).digest()
+        iv = cipher_text[:16]
+        encrypted_data = cipher_text[16:]
+        decipher = AES.new(key, AES.MODE_CBC, iv)
+        decipher_text = unpad(decipher.decrypt(encrypted_data), AES.block_size)
+        return decipher_text
+    except (ValueError, KeyError) as e:
+        print(f"Incorrect decryption. Error: {e}")
+
+
+def encrypted_text_processing(button_value: str, cipher_text: str, password):
+    """
+    Осуществляет получение данных из формы на сервере
+    @param button_value: Значение нажатой пользователем кнопки
+    @param cipher_text: Зашифрованный текст
+    @param password: Пароль, введеный пользователем
+    @return: Расшифрованный текст
+    """
+    if button_value == 'AESdcrptText':
+        decipher_text = aes_text_decryption(b64decode(cipher_text), password)
+        decipher_text = decipher_text.decode("utf-8")
+        return decipher_text
+    elif button_value == 'XORdcrptText':
+        cipher_text_b64 = b64decode(cipher_text)
+        decipher_text = xor_text_decryption(cipher_text_b64.decode("utf-8"), password)
+        return decipher_text
+
 
 def decryption(request):
-    return render(request, 'decipher/decryption.html')
+    """
+    Осуществляет получение расшифрованных данных из форм на сервере, определяет вводимые данные, удаляет файлы с сервера
+    @param request: Запрос
+    @return: Запрос, html-страница, словарь с выходными данными
+    """
+    media_path = Path(__file__).resolve().parent.parent / 'media'
+    for files in os.listdir(media_path):
+        path = os.path.join(media_path, files)
+        try:
+            shutil.rmtree(path)
+        except OSError:
+            os.remove(path)
+    if request.method == 'POST':
+        button_value = request.POST.get('button', False)
+        cipher_text = request.POST.get('encryptedText', False)
+        password = request.POST.get('textPassword', False)
+        if request.FILES.get('upload', False):
+            return file_processing(request, button_value)
+        else:
+            decipher_text = encrypted_text_processing(button_value, cipher_text, password)
+        return render(request, 'decipher/decryption.html',
+                      {'ciphertext': cipher_text,
+                       'textPassword': password,
+                       'deciphertext': decipher_text})
+    else:
+        return render(request, 'decipher/decryption.html')
--- encrypt/views.py
+++ encrypt/views.py
@@ -1,119 +1,227 @@
 import hashlib
-from os.path import isfile, join
-
+import mimetypes
+import os, shutil
+from base64 import b64encode
+from wsgiref.util import FileWrapper
+from django.http import StreamingHttpResponse
 from django.shortcuts import render
 from Crypto.Cipher import AES
 from django.core.files.storage import FileSystemStorage
-from os import listdir
-from .forms import FileForm
-from .models import File
+from pathlib import Path
+from Crypto.Util.Padding import pad
+
 
 def home(request):
+    """
+    Осуществляет загрузку домашней страницы
+    @param request: запрос
+    @return: запрос, html-страницу
+    """
     return render(request, 'encrypt/home.html')
 
-def fileSave(cipherdata,file_url):
-    with open(file_url + '_encrypted.bin', 'w+', encoding="utf-8") as newFile:
-        newFile.write(cipherdata)
-    return newFile
+
+def error_processing(text: str, password: str, placeholder_text: str, cipher_text: str, placeholder_password: str):
+    """
+    Осуществляет обработку ошибок ввода данных пользователем
+    @param text: Текст, вводимый пользователем
+    @param password: Пароль, вводимый пользователем
+    @param placeholder_text: Текст в строке ввода текста
+    @param cipher_text: Зашифрованный текст
+    @param placeholder_password: Текст в строке ввода пароля
+    @return: Ошибка в строке зашифрованного текста, ошибка в строке текста, ошибка в строке пароля, ошибка в строке текста, ошибка в строке пароля
+    """
+    if text == '' and password == '':
+        cipher_text = 'Nothing to encrypt :( \nPlease enter text to encrypt and password'
+        text = ''
+        password = ''
+    if text == '' and password != '':
+        placeholder_text = 'Please enter some text'
+        text = ''
+        cipher_text = 'Nothing to encrypt :( \nEnter text to encrypt'
+    elif text != '' and password == '':
+        placeholder_password = 'Please enter some password'
+        password = ''
+        cipher_text = 'Not enough data to encrypt your text :(\nEnter some password'
+    return cipher_text, text, password, placeholder_text, placeholder_password
 
 
-def aesFileEncryption(data,password,file_url):
+def download_file(file_url: str):
+    """
+    Осуществялет скачивание файла с сервера на устройство пользователя
+    @param file_url: Путь к файлу на сервере
+    @return: Данные, необходимые для скачивания файла
+    """
+    file = file_url
+    file_name = os.path.basename(file)
+    chunk_size = 8192
+    response = StreamingHttpResponse(FileWrapper(open(file, 'rb'), chunk_size),
+                                     content_type=mimetypes.guess_type(file)[0])
+    response['Content-Length'] = os.path.getsize(file)
+    response['Content-Disposition'] = "attachment; filename=%s" % file_name
+    return response
+
+
+def file_save(cipher_data: bytes, file_url: str):
+    """
+    Осуществялет сохранение файла на сервер
+    @param cipher_data: Зашифрованные данные
+    @param file_url: Путь к файлу на сервере
+    @return: Данные, необходимые для скачивания файла
+    """
+    file_url = file_url + '.encrypted'
+    with open(file_url, 'wb') as new_file:
+        new_file.write(cipher_data)
+    return download_file(file_url)
+
+
+def aes_file_encryption(binary_data: bytes, password: str, file_url: str):
+    """
+    Осуществялет шифрование файла с помощью алгоритма AES
+    @param binary_data: Данные в бинарном виде
+    @param password: Пароль, вводимый пользователем
+    @param file_url: Путь к файлу на сервере
+    @return: Данные, необходимые для скачивания файла
+    """
     key = hashlib.sha256(password.encode('utf-8')).digest()
-    cipher = AES.new(key, AES.MODE_EAX)
-    cipherdata, tag = cipher.encrypt_and_digest(data)
+    cipher = AES.new(key, AES.MODE_CBC)
+    cipher_data = cipher.encrypt(pad(binary_data, AES.block_size))
+    return file_save(cipher.iv + cipher_data, file_url)
 
-    nonce = cipher.nonce
-    #print('nonce =', nonce, 'tag = ', tag, 'key = ', key)
-    cipherdata = nonce.decode('utf-8', errors='ignore') + 'Н' + '\n' + tag.decode('utf-8', errors='ignore') + 'Т' + '\n' + cipherdata.decode('utf-8', errors='ignore')
-    print(cipherdata)
-    newFile = fileSave(cipherdata,file_url)
 
-    return newFile
+def xor_file_encryption(data: bytes, password: str, file_url: str):
+    """
+    Осуществялет шифрования файла с помощью алгоритма XOR
+    @param data: Данные в бинарном виде
+    @param password: Пароль, вводимый пользователем
+    @param file_url: путь к файлу на сервере
+    @return: Данные, необходимые для скачивания файла
+    """
+    try:
+        encrypted_data = []
+        for i in range(len(data)):
+            encrypted_data.append(data[i] ^ ord(password[i % len(password)]))
+        cipher_data = bytes(encrypted_data)
+        return file_save(cipher_data, file_url)
+    except ZeroDivisionError:
+        return "You did't enter encryption password.It is impossible to encrypt file"
 
-def xorFileEncryption(data,password,file_url):
-    encrypted_data = []
-    cipherdata = ''
-    nonce = ''
-    tag = ''
 
-    key = bytes(password, encoding='utf-8')
-    for i in range(len(data)):
-        encrypted_data.append(data[i] ^ key[i % len(key)])
-        cipherdata += chr(encrypted_data[i])
-
-    newFile = fileSave(cipherdata, file_url)
-
-    return newFile
-
-def fileProcessing(request, buttonValue):
+def file_processing(request, button_value: str):
+    """
+    Осуществляет получение данных из файла
+    @param request: Запрос
+    @param button_value: Значение нажатой кнопки
+    @return: Данные, необходимые для скачивания файла
+    """
     upload = request.FILES.get('upload', False)
-
     fss = FileSystemStorage()
     file = fss.save(upload.name, upload)
     file_url = fss.url(file)[1:]
 
-    with open(file_url,'rb') as file:
+    with open(file_url, 'rb') as file:
         data = file.read()
-
     password = request.POST.get('filePassword')
 
-    if buttonValue == 'AESpressedFile':
-        newFile = aesFileEncryption(data,password,file_url)
-        return newFile, password
+    if button_value == 'AESpressedFile':
+        return aes_file_encryption(data, password, file_url)
 
-    elif buttonValue == 'XORpressedFile':
-        newFile = xorFileEncryption(data,password,file_url)
-        return newFile, password
+    elif button_value == 'XORpressedFile':
+        return xor_file_encryption(data, password, file_url)
 
-def aesTextEncryption(text,password):
-    data = text.encode('utf-8')
 
+def aes_text_encryption(binary_data: bytes, password: str):
+    """
+    Осуществялет AES шифрование текста
+    @param binary_data: данные в бинарном виде
+    @param password: пароль для шифрования
+    @return:строка, содержащая синициализирующий вектор и зашифрованный текст
+    """
     key = hashlib.sha256(password.encode('utf-8')).digest()
+    cipher = AES.new(key, AES.MODE_CBC)
+    cipher_text = cipher.encrypt(pad(binary_data, AES.block_size))
+    iv = cipher.iv
+    return iv + cipher_text
 
-    cipher = AES.new(key, AES.MODE_EAX)
-    cipherdata, tag = cipher.encrypt_and_digest(data)
 
-    nonce = cipher.nonce
-    # print('nonce =', nonce, 'tag = ', tag, 'key = ', key)
-    cipherdata = nonce.decode('utf-8', errors='ignore') + 'Н' + '\n' + tag.decode('utf-8', errors='ignore') + 'Т' + '\n' + cipherdata.decode('utf-8', errors='ignore')
-    #print('\nnonce = ' + nonce.decode('utf-8', errors='ignore') + '\ntag = ' + tag.decode('utf-8', errors='ignore'))
-    return data.decode('utf-8', errors='ignore'), cipherdata
+def xor_text_encryption(text: str, password: str):
+    """
+    Осуществляет шифрование текстовых данных
+    :param text: исходный текст
+    :param password: пароль для шифрования
+    :return: зашифрованный текст
+    """
+    try:
+        encrypted_data = []
+        for i in range(len(text)):
+            encrypted_data.append(ord(text[i]) ^ ord(password[i % len(password)]))
 
-def xorTextEncryption(text,password):
-    encrypted_data = []
-    cipherdata = ''
-    data = bytes(text, encoding='utf-8')
-    key = bytes(password, encoding='utf-8')
-    for i in range(len(data)):
-        encrypted_data.append(data[i] ^ key[i % len(key)])
-        cipherdata += chr(encrypted_data[i])
-    # print('key =', key)
-    return cipherdata
+        encrypted_text = ''
+        for i in range(len(encrypted_data)):
+            encrypted_text = encrypted_text + chr(encrypted_data[i])
 
-def textProcessing(buttonValue,text,password):
-        if buttonValue == 'AESpressedText':
-            data,cipherdata = aesTextEncryption(text,password)
-            return cipherdata
-        elif buttonValue == 'XORpressedText':
-            cipherdata = xorTextEncryption(text,password)
-            return cipherdata
+        return encrypted_text
+    except ZeroDivisionError:
+        return 'Not enough data to encrypt your text :(\nEnter some password'
+
+
+def text_processing(button_value, text, password):
+    """
+    Осуществляет получение данных из формы на сервере
+    @param button_value: Значение нажатой пользователем кнопки
+    @param text: Текст, введеный пользователем
+    @param password: Пароль введеный пользователем
+    @return: Зашифрованный текст
+    """
+    if button_value == 'AESpressedText':
+        cipher_text = aes_text_encryption(text.encode('utf-8'), password)
+        cipher_text = b64encode(cipher_text).decode('utf-8')
+        return cipher_text
+    elif button_value == 'XORpressedText':
+        cipher_text = xor_text_encryption(text, password)
+        cipher_text = b64encode(cipher_text.encode('utf-8')).decode('utf-8')
+        return cipher_text
+
 
 def encryption(request):
-    filePassword = ''
-    cipherdata = ''
-    newFile = ''
+    """
+    Осуществляет получение данных из форм на сервере, определяет вводимые данные, удаляет файлы с сервера
+    @param request: Запрос с сервера
+    @return: Запрос, html-страница, словарь с выходными данными
+    """
+    placeholder_text = "Enter your text"
+    placeholder_password = 'Enter your password'
+    file_password_placeholder = "Enter your password"
+
+    media_path = Path(__file__).resolve().parent.parent / 'media'
+    for files in os.listdir(media_path):
+        path = os.path.join(media_path, files)
+        try:
+            shutil.rmtree(path)
+        except OSError:
+            os.remove(path)
+
     if request.method == 'POST':
-        buttonValue = request.POST.get('button')
+        button_value = request.POST.get('button')
         text = request.POST.get('text')
         password = request.POST.get('textPassword')
-        if text == None and password == None:
-            text = ''
-            password = ''
         if request.FILES.get('upload', False):
-            newFile,filePassword = fileProcessing(request, buttonValue)
+            return file_processing(request, button_value)
         else:
-            cipherdata = textProcessing(buttonValue,text,password)
-        return render(request, 'encrypt/encryption.html', {'text':text, 'textPassword':password, 'cipherdata': cipherdata, 'newFile':newFile,'filePassword':filePassword})
-    else:
-        return render(request, 'encrypt/encryption.html')
+            cipher_text = text_processing(button_value, text, password)
 
+        cipher_text, text, password, placeholder_text, placeholder_password = error_processing(text, password,
+                                                                                               placeholder_text,
+                                                                                               cipher_text,
+                                                                                               placeholder_password)
+
+        return render(request, 'encrypt/encryption.html',
+                      {'text': text,
+                       'textPassword': password,
+                       'ciphertext': cipher_text,
+                       'placeholder_text': placeholder_text,
+                       'placeholder_password': placeholder_password})
+    else:
+        return render(request, 'encrypt/encryption.html',
+                      {'placeholder_text': placeholder_text,
+                       'placeholder_password': placeholder_password,
+                       'file_password_placeholder': file_password_placeholder})
